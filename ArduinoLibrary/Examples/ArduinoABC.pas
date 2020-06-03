c21fa1f487053069a98255690346d71430adafa7
Avilov Alexandr <sanya.a.97@yandex.ru>
2020-06-03 18:08:19 +0300

    More userfriendly ABC unit

--- ArduinoLibrary/Examples/ArduinoABC.pas
+++ ArduinoLibrary/Examples/ArduinoABC.pas
@@ -1,12 +1,22 @@
 ﻿unit ArduinoABC;
 {$reference ..\ResultDll\ArduinoLibrary.dll}
 
+//Упрощение доступа к классу Arduino
 type
   Arduino = ArduinoLibrary.Arduino;
 
+//Упрощение доступа к перечислимому типу PinMode
+type
+  PinMode = ArduinoLibrary.Arduino.PinMode;
+
+//Константы режимов работы без использования перечислимых типов
 const
+  ///Аналоговый вход
   AnalogInput = Arduino.PinMode.AnalogInput;
+  ///Аналоговый выход
   AnalogOutput = Arduino.PinMode.AnalogOutput;
+  ///Дискретный вход
   DigitalInput = Arduino.PinMode.DigitalInput;
+  ///Дискретный выход
   DigitalOutput = Arduino.PinMode.DigitalOutput;
 end.
